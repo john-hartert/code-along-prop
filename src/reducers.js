cff60bc0c31a219ceb3c138f2ae97438281d269f
John Hartert <j.blainehartert@gmail.com>
2017-09-13 13:49:07 -0400

    Adds more code for redux work in reducers.js and actions.js

--- src/reducers.js
+++ src/reducers.js
@@ -4,9 +4,14 @@ import actions from './actions';
 const crewMembers = (state=[], action) => {
     switch (action.type) {
         case actions.ADD_MEMBER:
-            
+            return state.concat({
+                name: action.name,
+                rank: action.rank,
+                id: action.id
+            });
+        default:
+            return state;
+        }
     }
-    return state;
-}
 
 export default crewMembers;
