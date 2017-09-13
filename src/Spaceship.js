74787e6e8f19f0aab82437173a01bbfc60871e49
John Hartert <j.blainehartert@gmail.com>
2017-09-13 10:31:16 -0400

    Makes changes to App.js

--- src/Spaceship.js
+++ src/Spaceship.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 // Render is the only lifecycle method required for a class component.
 class Spaceship extends Component {
+    static propTypes = {
+        name: PropTypes.string
+    };
+
     render() {
+       const { name } = this.props;
+
+       return (
         <div>
-            <h1>{this.props.name}</h1>
+            <h1>{name}</h1>
         </div>
+       );
     }
 }
+
+export default Spaceship;
