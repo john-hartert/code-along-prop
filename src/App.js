ca0ff0ee6aae4e006e97e1f8e9f8c7505055eb03
John Hartert <j.blainehartert@gmail.com>
2017-09-13 11:24:03 -0400

    Removes stock create react app css and centers everything

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Spaceship from './Spaceship';
@@ -11,8 +10,6 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <Spaceship name = 'Enterprise' 
         commanderType = 'CrewMember'>
