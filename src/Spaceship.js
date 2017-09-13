44798e0bfdcdb5752e067f369f8d4be8ffa3edb7
John Hartert <j.blainehartert@gmail.com>
2017-09-13 10:23:58 -0400

    Adds render method

---
+++ src/Spaceship.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+// Render is the only lifecycle method required for a class component.
+class Spaceship extends Component {
+    render() {
+        <div>
+            <h1>{this.props.name}</h1>
+        </div>
+    }
+}
