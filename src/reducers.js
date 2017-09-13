5282b585fc8e44e3a0936da0641dfca58b942e31
John Hartert <j.blainehartert@gmail.com>
2017-09-13 14:27:16 -0400

    Fixes bugs

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,11 @@
 // State and action are always passed in reducers.
 import actions from './actions';
 
-
+export const LOCATIONS = {
+    TRANSPORTER_ROOM: 'TRANSPORTER_ROOM',
+    PLANET_EARTH: 'PLANET_EARTH',
+    PLANET_THE_SUN: 'PLANET_THE_SUN'
+}
 
 const crewMembers = (state=[], action) => {
     switch (action.type) {
@@ -15,12 +19,15 @@ const crewMembers = (state=[], action) => {
         case actions.BEAM_MEMBER:
             return state.map( (crewMember) => {
                 // if the id matches, then update the location, ottherwise just return the crewMember.
-                if (action.id === crewMember.id) {
+                if (action.id === crewMember.id && action.location in LOCATIONS) {
                     //make a copy maybe???? of the crewMember
                     //but changing the location
+                    // if (action.location in LOCATIONS) {
+
+                    // }
                     return Object.assign({}, crewMember, {
                         location: action.location
-                    })
+                    });
                 } else {
                     return crewMember;
                 }
